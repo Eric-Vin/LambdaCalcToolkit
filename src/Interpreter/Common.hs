934e500a79ee1eb85b914d798a6f3bd52e2ff80d
Eric Vin <ericvin@outlook.com>
2022-02-24 17:17:35 -0800

    Added definitions for ints and bools.

--- src/Interpreter/Common.hs
+++ src/Interpreter/Common.hs
@@ -9,7 +9,14 @@ data LambdaExpr = Function [LambdaVar] [LambdaExpr]
 
 -- Encoding Helper Functions
 encodeInt :: Int -> LambdaExpr
-encodeInt i   = undefined
+encodeInt i   = Function [func_var, input_var] lexpr 
+    where
+        func_var    = Var "f"
+        input_var   = Var "x"
+        lexpr i     =   if i == 0
+                        then input_var
+                        else func_var:(lexpr (i-1))
 
 encodeBool :: Bool -> LambdaExpr
-encodeBool b  = undefined
+encodeBool True     = Function [(Var "x"), (Var "y")] [(Var "x")]
+encodeBool False    = Function [(Var "x"), (Var "y")] [(Var "y")]
